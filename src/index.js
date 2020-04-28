4a798fa603824c0569e4e3157148e14f6c41f87f
PiotrGrobelak <grobelak.piotr@gmail.com>
2020-04-28 11:50:59 +0200

    feat: clean code and add default src structure

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import Root from 'views/Root';
+import Main from 'views/Main';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Root />
+    <Main />
   </React.StrictMode>,
   document.getElementById('root'),
 );
---
+++ src/views/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Main() {
+  return (
+    <div className="Main">
+      <h1>Hello App</h1>
+    </div>
+  );
+}
+
+export default Main;
